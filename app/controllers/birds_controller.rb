31a2e74898bb62f4f51e0ced673afe81701130e3
puritymwaura <purity.mwaura@student.moringaschool.com>
2022-09-27 12:58:10 +0300

    done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,5 @@
 class BirdsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /birds
   def index
@@ -14,23 +15,19 @@ class BirdsController < ApplicationController
 
   # GET /birds/:id
   def show
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    bird = find_bird
+    render json: bird
+  rescue ActiveRecord::RecordNotFound
+    render_not_found_response
   end
 
   # PATCH /birds/:id
   def update
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      bird.update(bird_params)
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    bird = find_bird
+    bird.update(bird_params)
+    render json: bird
+  rescue ActiveRecord::RecordNotFound
+    render_not_found_response
   end
 
   # PATCH /birds/:id/like
@@ -57,8 +54,11 @@ class BirdsController < ApplicationController
 
   private
 
-  def bird_params
-    params.permit(:name, :species, :likes)
+  def find_bird
+    Bird.find(params[:id])
   end
 
+def render_not_found_response
+  render json: { error: "Bird not found" }, status: :not_found
+end
 end
